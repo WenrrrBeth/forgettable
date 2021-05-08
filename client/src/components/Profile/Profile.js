b78e28351006fce951a975625ba538d3198c8998
WenrrrBeth <bethding0923@gmail.com>
2021-05-07 22:45:28 -0700

    profile component added, border design fixed

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,15 +1,34 @@
-import { Container } from '@material-ui/core';
-import React from 'react';
-import useStyles from './styles.js';
+import { Container, Grid, Typography, Paper } from "@material-ui/core";
+import React, { useState } from "react";
+import useStyles from "./styles.js";
+import bgimg from "../../designs/bgimg_defaultDesign.png";
+import bgimgBorder from "../../designs/bgimg_BorderDesign.PNG";
+import "../../fonts.css";
 
 const Profile = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const [subNav, setSubNav] = useState(0);
+  const user = JSON.parse(localStorage.getItem("profile"));
 
-    return (
-        <Container className={classes.profileContainer}>
-            Profile Page
-        </Container>
-    )
-}
+  return (
+    <Container className={classes.profileContainer}>
+      <Grid className={classes.profile} container>
+        <Typography className={classes.title} variant="h6">
+          Profile
+        </Typography>
+        <img
+          className={classes.bgimg}
+          src={user.bgimg ? user.bgimg : bgimg}
+          alt="default background image design"
+        />
+        <img
+          className={classes.bgimgOverlay}
+          src={bgimgBorder}
+          alt="background image border design"
+        />
+      </Grid>
+    </Container>
+  );
+};
 
-export default Profile
+export default Profile;
--- client/src/components/Profile/styles.js
+++ client/src/components/Profile/styles.js
@@ -1,9 +1,41 @@
 import { makeStyles } from "@material-ui/core/styles";
+import bgimgBorder from "../../designs/bgimg_BorderDesign.PNG";
 
-const theme = makeStyles({
-  profileContainer:{
-      marginTop: "80px",
-  }
-});
+const theme = makeStyles((theme) => ({
+  profileContainer: {
+    marginTop: "60px",
+    alignItems: "center",
+  },
+  profile: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+  title: {
+    fontFamily: "'Abril Fatface', cursive",
+    zIndex: "1000",
+  },
+  bgimg: {
+    objectFit: "cover",
+    width: "80%",
+    height: "70vh",
+    position: "relative",
+  },
+  bgimgOverlay: {
+    marginTop: "-10px",
+    width: "80%",
+    height: "77vh",
+    margin: 0,
+    position: "absolute",
+    objectFit: "fill",
+  },
+
+  [theme.breakpoints.down("sm")]: {
+    bgimg: {
+      width: "100%",
+    },
+  },
+}));
 
 export default theme;
