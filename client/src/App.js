764b8b1489bf9b6348b94ca62cd0db546be990c1
WenrrrBeth <bethding0923@gmail.com>
2021-05-09 23:02:10 -0700

    removed unnecessary code

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,9 @@ import Home from "./components/Home/Home";
 import Postform from "./components/Write/Postform";
 import Authentication from "./components/Authentication/Authentication";
 import Profile from "./components/Profile/Profile";
+import Settings from "./components/Settings/Settings";
 
 function App() {
-  const user = JSON.parse(localStorage.getItem("profile"));
-
   return (
     <BrowserRouter>
       <Container maxwidth="lg">
@@ -20,6 +19,7 @@ function App() {
           <Route path="/write" exact component={Postform} />
           <Route path="/profile" exact component={Profile} />
           <Route path="/profile/authenticate" exact component={Authentication} />
+          <Route path="/profile/settings" exact component={Settings} />
         </Switch>
       </Container>
     </BrowserRouter>
