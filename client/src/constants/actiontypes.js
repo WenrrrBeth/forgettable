97bf244b36979fd6e0a10ca73d9c85916919ceea
WenrrrBeth <bethding0923@gmail.com>
2021-05-14 23:39:05 -0700

    constants updated

--- client/src/constants/actiontypes.js
+++ client/src/constants/actiontypes.js
@@ -6,3 +6,5 @@ export const FETCH_ONE = "FETCH_ONE";
 export const POST = "POST";
 export const FETCH_UNSHARED = "FETCH_UNSHARED";
 export const FETCH_SHARED = "FETCH_SHARED";
+export const FETCH_ALL_SHARED = "FETCH_ALL_SHARED";
+export const SAVE = "SAVE";
